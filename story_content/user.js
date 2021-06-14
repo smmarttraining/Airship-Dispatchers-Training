04295c8cc9730a4efd702ace15581d0c2e16a911
aoropio <andrewmark.a.oropio@smretail.com>
2021-06-14 15:23:20 +0800

    June 14 v2

---
+++ story_content/user.js
@@ -0,0 +1,47 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5wUKVJw0DAH":
+        Script1();
+        break;
+      case "5gPV0IGR91X":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  //load the scripts dynamically into the head of the document
+function add_line() {
+    var line = document.createElement("audio");
+    var head=document.getElementsByTagName('body')[0];
+    line.type = "audio/mp3";
+    line.src="";
+    line.id="bgSong" ;
+	line.autoplay = true;
+	line.loop = true;
+    head.appendChild(line);
+}
+
+//but we only want to add these once!
+if(document.getElementById('bgSong')==null){
+	add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.03;
+}
+
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+
+var audio = document.getElementById('bgSong');
+audio.src=Location+"corporate1.mp3";
+audio.load();
+audio.play();
+}
+
