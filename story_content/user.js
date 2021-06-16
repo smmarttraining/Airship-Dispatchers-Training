47fd3426cea00cabe69e0a2130d85c570648ef0f
aoropio <andrewmark.a.oropio@smretail.com>
2021-06-16 16:40:10 +0800

    June 16 relink

--- story_content/user.js
+++ story_content/user.js
@@ -2,46 +2,6 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6IGrd61hhmj":
-        Script1();
-        break;
-      case "5lKbUtPEI1e":
-        Script2();
-        break;
   }
-}
-
-function Script1()
-{
-  //load the scripts dynamically into the head of the document
-function add_line() {
-    var line = document.createElement("audio");
-    var head=document.getElementsByTagName('body')[0];
-    line.type = "audio/mp3";
-    line.src="";
-    line.id="bgSong" ;
-	line.autoplay = true;
-	line.loop = true;
-    head.appendChild(line);
-}
-
-//but we only want to add these once!
-if(document.getElementById('bgSong')==null){
-	add_line();
-var audio = document.getElementById('bgSong');
-audio.volume = 0.03;
-}
-
-}
-
-function Script2()
-{
-  var player = GetPlayer();
-this.Location= player.GetVar("location");
-
-var audio = document.getElementById('bgSong');
-audio.src=Location+"corporate1.mp3";
-audio.load();
-audio.play();
 }
 
